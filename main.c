c211d914a6b5bb79a524c99f19248af99e00eeab
Alice VEDRENNE <avedrenn@e1r7p12.clusters.42paris.fr>
2023-04-06 17:01:11 +0200

    start

--- main.c
+++ main.c
@@ -6,7 +6,27 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:08:42 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/04/04 15:08:43 by avedrenn         ###   ########.fr       */
+/*   Updated: 2023/04/06 16:27:22 by avedrenn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+
+
+
+int	main()
+{
+	char	*line;
+
+	sigaction(SIGINT, sig_handler);
+	while (1)
+	{
+		line = readline("minishell>");
+		if (!line)
+			break ;
+		printf("%s\n", line);
+		//parse_line(line);
+	}
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 13:33:27 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/04/06 16:27:18 by avedrenn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//includes
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+//Compile the executable with libreadline (-lreadline)
+
+enum e_types
+{
+	METACHARACTERS = 1 << 0, //Any metacharacter except ;
+	PIPE = 1 << 1, // | and newline
+	OUTPUT_REDIRECTION = 1 << 2,
+	INPUT_REDIRECTION = 1 << 3,
+	DOUBLE_INPUT_REDIR = 1 << 4,
+	APPEND_REDIRECTION = 1 << 5,
+	ENV_VARIABLE = 1 << 6,
+	EXIT_STATUS = 1 << 7, // $?
+};
+
+struct s_arg
+{
+	char			*argument;
+	enum e_types	type;
+};
+
+/* struct	s_data
+{
+	t_list	*args;
+	t_list	*infiles;
+	t_list	*outfiles;
+	t_list	*commands;
+} */
+
+#endif
