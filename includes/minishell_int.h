828f28e5285391bc8877183592c60aaa3b4f1e44
nsainton <nsainton@student.42.fr>
2023-04-09 09:29:31 +0200

    Moved minishell.h to minishell_int.h

--- includes/minishell_int.h
+++ includes/minishell_int.h
@@ -3,14 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_int.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 18:13:34 by nsainton          #+#    #+#             */
-/*   Updated: 2023/04/08 18:13:55 by nsainton         ###   ########.fr       */
+/*   Created: 2023/04/06 13:33:27 by avedrenn          #+#    #+#             */
+/*   Updated: 2023/04/09 09:11:52 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//includes
 #ifndef MINISHELL_INT_H
 # define MINISHELL_INT_H
-# include "libft.h"
+
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+//Compile the executable with libreadline (-lreadline)
+
+enum e_types
+{
+	METACHARACTERS = 1 << 0, //Any metacharacter except ;
+	PIPE = 1 << 1, // | and newline
+	OUTPUT_REDIRECTION = 1 << 2,
+	INPUT_REDIRECTION = 1 << 3,
+	DOUBLE_INPUT_REDIR = 1 << 4,
+	APPEND_REDIRECTION = 1 << 5,
+	ENV_VARIABLE = 1 << 6,
+	EXIT_STATUS = 1 << 7, // $?
+};
+
+struct s_arg
+{
+	char			*argument;
+	enum e_types	type;
+};
+
+/* struct	s_data
+{
+	t_list	*args;
+	t_list	*infiles;
+	t_list	*outfiles;
+	t_list	*commands;
+} */
+
 #endif
