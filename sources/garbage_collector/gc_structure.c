987c0d9540eb1bf7d9151e6aab17f5109946408b
nsainton <nsainton@student.42.fr>
2023-04-12 11:25:22 +0200

    Started functions for gc_structure (alloc and free)

--- sources/garbage_collector/gc_structure.c
+++ sources/garbage_collector/gc_structure.c
@@ -6,10 +6,44 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 10:53:57 by nsainton          #+#    #+#             */
-/*   Updated: 2023/04/12 10:54:16 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/04/12 11:25:14 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_gc	*getgc(void)
+{
+	static t_gc	collector;
 
+	if (collector.memzones)
+		return (&collector);
+	collector.memzones = malloc(sizeof * collector.memzones * TRASH_SIZE);
+	if (! collector.memzones)
+	{
+		ft_bzero(&collector, sizeof collector);
+		return (NULL);
+	}
+	collector.size = TRASH_SIZE;
+	collector.len = 0;
+	return (collector);
+}
+
+int	free_gc(t_cint errcode)
+{
+	t_gc	*collector;
+	size_t	index;
+
+	collector = getgc();
+	if (! collector)
+		return (errcode);
+	index = 0;
+	while (index < collector->len);
+	{
+		free(*(collector->memzones + index));
+		index ++;
+	}
+	free(collector->memzones);
+	ft_bzero(collector);
+	return (errcode);
+}
