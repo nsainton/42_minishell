2b142562686c779b41db7b4d0935df1e96588045
nsainton <nsainton@student.42.fr>
2023-06-25 22:48:16 +0200

    Finished implementation of exit builtin

--- sources/builtin/exit.c
+++ sources/builtin/exit.c
@@ -6,23 +6,28 @@
 /*   By: nsainton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 23:52:52 by nsainton          #+#    #+#             */
-/*   Updated: 2023/06/23 21:21:48 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/06/24 16:23:40 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
 static t_ll	store_status(const t_ll new_status)
 {
 	static t_ll	status;
 	t_ll		tmp;
 
+	EPRINT;
 	tmp = status;
 	status = new_status;
+	ft_printf("This is the old status : %d\n", tmp);
+	ft_printf("This is the new status : %d\n", status);
 	if (! tmp)
 		return (status);
 	return (tmp);
 }
+*/
 
 static int	check_args(char **args, t_csizet len)
 {
@@ -31,21 +36,31 @@ static int	check_args(char **args, t_csizet len)
 
 	status = (t_ll)strtoll_errors(*args, DEC, &err);
 	ft_dprintf(STDERR_FILENO, "exit\n");
+	//ft_dprintf(STDERR_FILENO, "This is the len : %u\n", len);
 	if (err)
 	{
+		ft_dprintf(STDERR_FILENO, "Error\n");
 		ft_dprintf(STDERR_FILENO, "minishell: exit: %s: %s\n", *args, \
 		STRING_NUMERIC_REQUIRED);
-		exit_free_gc(store_status(NUMERIC_REQUIRED));
+		keep_exit_status(NUMERIC_REQUIRED);
+		exit_free_gc(keep_exit_status(-1));
 	}
 	if (len == 1)
-		exit_free_gc(store_status(status % 256));
-	if (len > 2)
 	{
+		ft_dprintf(STDERR_FILENO, "No error and only one argument\n");
+		keep_exit_status(status % 256);
+		exit_free_gc(keep_exit_status(-1));
+	}
+	if (len > 1)
+	{
+		ft_dprintf(STDERR_FILENO, "No error but multiple arguments\n");
 		ft_dprintf(STDERR_FILENO, "minishell: exit: %s\n", \
 		STRING_TOO_MANY);
-		keep_exit_status(store_status(TOO_MANY_ARGUMENTS));
+		ft_printf("This is the status stored : %d\n", TOO_MANY_ARGUMENTS);
+		keep_exit_status(TOO_MANY_ARGUMENTS);
+		//return (store_status(TOO_MANY_ARGUMENTS));
 	}
-	return (store_status(EXIT_SUCCESS));
+	return (keep_exit_status(-1));
 }
 
 int	exit_builtin(char **args)
@@ -53,9 +68,16 @@ int	exit_builtin(char **args)
 	size_t	len;
 
 	if (args == NULL)
-		exit_free_gc(store_status(EXIT_SUCCESS));
+	{
+		ft_printf("Args is NULL\n");
+		exit_free_gc(keep_exit_status(-1));
+	}
 	len = tablen(args, sizeof * args);
 	if (! len)
-		exit_free_gc(store_status(EXIT_SUCCESS));
+	{
+		ft_printf("No arguments\n");
+		exit_free_gc(keep_exit_status(-1));
+	}
+	//ft_dprintf(2, "This is the len : %u\n", len);
 	return (check_args(args, len));
 }
