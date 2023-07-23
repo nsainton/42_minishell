326ac33fb591bb725a39474c87bf35753edc2a7b
nsainton <nsainton@student.42.fr>
2023-07-23 10:30:47 +0200

    Normed get_heredoc

--- sources/exec/get_heredoc.c
+++ sources/exec/get_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 11:38:38 by nsainton          #+#    #+#             */
-/*   Updated: 2023/07/21 11:38:41 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/07/23 10:30:06 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,68 +14,67 @@
 
 static int	read_heredoc(const char *limiter, int write_fd)
 {
-  char			*line;
-  static unsigned int	line_index;
+	char				*line;
+	static unsigned int	line_index;
 
-  line_index++;
-  line = readline(HEREDOC_PROMPT);
-  while (line && ft_strcmp(line, limiter))
-    {
-      if (ft_putendl_fd(line, write_fd) == -1)
-	return (EXIT_FAILURE);
-      free(line);
-      line = readline(HEREDOC_PROMPT);
-      line_index ++;
-    }
-  if (! line)
-    ft_dprintf(STDERR_FILENO, HEREDOC_WARNING, line_index, limiter);
-  close(write_fd);
-  //ft_printf("Ich funktioniere\n");
-  return ((line != NULL) - 1);
+	line_index++;
+	line = readline(HEREDOC_PROMPT);
+	while (line && ft_strcmp(line, limiter))
+	{
+		if (ft_putendl_fd(line, write_fd) == -1)
+			return (EXIT_FAILURE);
+		free(line);
+		line = readline(HEREDOC_PROMPT);
+		line_index ++;
+	}
+	if (! line)
+		ft_dprintf(STDERR_FILENO, HEREDOC_WARNING, line_index, limiter);
+	close(write_fd);
+	return ((line != NULL) - 1);
 }
 
 static int	heredoc_open(const char *heredoc_name, struct s_heredoc_infos *hd)
 {
-  hd->write_fd = open(heredoc_name, O_EXCL | O_CREAT | O_WRONLY | O_TRUNC, \
-  00600);
-  if (hd->write_fd == -1)
-    return (EXIT_FAILURE);
-  hd->read_fd = open(heredoc_name, O_RDONLY);
-  if (hd->read_fd == -1)
-    {
-      close(hd->write_fd);
-      return (EXIT_FAILURE);
-    }
-  if (unlink(heredoc_name) == -1)
-    {
-      close(hd->read_fd);
-      close(hd->write_fd);
-      return (EXIT_FAILURE);
-    }
-  return (EXIT_SUCCESS);
+	hd->write_fd = open(heredoc_name, O_EXCL | O_CREAT | O_WRONLY | O_TRUNC, \
+	00600);
+	if (hd->write_fd == -1)
+		return (EXIT_FAILURE);
+	hd->read_fd = open(heredoc_name, O_RDONLY);
+	if (hd->read_fd == -1)
+	{
+		close(hd->write_fd);
+		return (EXIT_FAILURE);
+	}
+	if (unlink(heredoc_name) == -1)
+	{
+		close(hd->read_fd);
+		close(hd->write_fd);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
 static void	create_random_name(char *name, void *address)
 {
-  unsigned int	random;
-  char		*tmp;
+	unsigned int	random;
+	char			*tmp;
 
-  *name = 0;
-  tmp = name;
-  tmp += ft_strncat(name, HEREDOC, 100);
-  random = (unsigned int)((uintptr_t)address & 0xffffffff);
-  put_uns_tab(random, &tmp, HEX, random % 10 + 5);
+	*name = 0;
+	tmp = name;
+	tmp += ft_strncat(name, HEREDOC, 100);
+	random = (unsigned int)((uintptr_t)address & 0xffffffff);
+	put_uns_tab(random, &tmp, HEX, random % 10 + 5);
 }
 
 int	getheredoc(struct s_heredoc_infos *hd, struct s_heredoc *heredoc)
 {
-  char	name[100];
-  int	error;
+	char	name[100];
+	int		error;
 
-  ft_bzero(hd, sizeof * hd);
-  create_random_name(name, heredoc);
-  if (heredoc_open(name, hd))
-    return (EXIT_FAILURE);
-  error = read_heredoc(heredoc->limiter, hd->write_fd);
-  return ((error > 0) * EXIT_FAILURE + (error < 1) * error);
+	ft_bzero(hd, sizeof * hd);
+	create_random_name(name, heredoc);
+	if (heredoc_open(name, hd))
+		return (EXIT_FAILURE);
+	error = read_heredoc(heredoc->limiter, hd->write_fd);
+	return ((error > 0) * EXIT_FAILURE + (error < 1) * error);
 }
