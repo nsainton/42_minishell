5f82c5da964278cfd587f377c81f77b9844200e3
nsainton <nsainton@student.42.fr>
2023-08-10 13:01:33 +0200

    Started modifying heredoc.c so that I will be able to integrate the command_index inside

--- sources/exec/heredocs/heredoc.c
+++ sources/exec/heredocs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 10:33:31 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/10 10:58:12 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/10 12:59:37 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,24 @@ static void	init_list(int *descriptors_list, size_t size)
 	}
 }
 
-static int	match_fds(int *descriptors_list)
+static int	match_fd(int *command_fd, int *heredoc_fd)
+{
+	if (dup2(*heredoc_fd, *command_fd) == -1)
+	{
+		perror("dup2");
+		clear_list();
+		return (1);
+	}
+	if (close(*heredoc_fd))
+	{
+		*heredoc_fd = -1;
+		clear_list();
+		return (1);
+	}
+	return (0);
+}
+
+static int	match_fds(int *descriptors_list, const int command_index)
 {
 	char			nullelem[20];
 	size_t			i;
@@ -38,8 +55,18 @@ static int	match_fds(int *descriptors_list)
 	ft_bzero(nullelem, elem_number * sizeof * descriptors_list);
 	i = 0;
 	while (ft_memcmp(descriptors_list + i, nullelem, \
-	elem_number * sizeof * descriptors_list) && *(descriptors_list + i) != -1)
+	elem_number * sizeof * descriptors_list) \
+	&& *(descriptors_list + i + 2) != -1 \
+	&& *(descriptors_list + i + 2) < command_index)
+		i += elem_number;
+	while (ft_memcmp(descriptors_list + i, nullelem, \
+	elem_number * sizeof * descriptors_list) \
+	&& *(descriptors_list + i) != -1 \
+	&& *(decriptors_list + i + 2) == command_index)
 	{
+		if (match_fd(descriptors_list + i, descriptors_list + i + 1))
+			return (1);
+		/*
 		if (dup2(*(descriptors_list + i + 1), *(descriptors_list + i)) == -1)
 		{
 			perror("dup2");
@@ -52,6 +79,7 @@ static int	match_fds(int *descriptors_list)
 			clear_list();
 			return (1);
 		}
+		*/
 		*(descriptors_list + i + 1) = *(descriptors_list + i);
 		i += elem_number;
 	}
@@ -69,7 +97,8 @@ int	heredoc(const struct s_command *command, const struct s_env *env)
 	len = tablen(*command->heredocs, sizeof **command->heredocs);
 	if (! len)
 		return (EXIT_SUCCESS);
-	descriptors_list = getlist(len + 1, elem_number * sizeof * descriptors_list);
+	descriptors_list = getlist(len + 1, \
+	elem_number * sizeof * descriptors_list);
 	if (! descriptors_list)
 		return (1);
 	init_list(descriptors_list, elem_number * len);
