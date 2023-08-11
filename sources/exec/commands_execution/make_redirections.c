5cb8b6a77e1c71cbe44b6f1fc8b21f72f0c16b23
nsainton <nsainton@student.42.fr>
2023-08-11 15:21:41 +0200

    :construction: Started make redirections function :construction:

---
+++ sources/exec/commands_execution/make_redirections.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   make_redirections.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/11 14:47:59 by nsainton          #+#    #+#             */
+/*   Updated: 2023/08/11 15:02:48 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+* @brief Makes the redirections for the given command
+*
+* @params[in] command : A struct s_ncommand variable that holds the command
+* we are looking at.
+*
+* @return An integer that is null if everything went well and that 
+* is positive otherwise
+*/
+/*
+int	make_redirections(struct s_ncommand *command)
+{
+*/
