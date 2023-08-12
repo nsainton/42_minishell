85eab01402e2f76c640501985bffe69f7a9fdbc0
nsainton <nsainton@student.42.fr>
2023-08-12 13:05:05 +0200

    :construction: Added function check_hashmap that checks if an executable is in the hash table and take appropriate action :construction:

--- sources/exec/commands_execution/get_path.c
+++ sources/exec/commands_execution/get_path.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 10:48:37 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/12 11:23:44 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/12 13:03:55 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,38 @@ static int	find_in_path(const char *command, const char **path)
 }
 */
 
-/*
+int	is_fullname(const char *path, const char *name)
+{
+	const char	*varname;
+
+	varname = ft_strrchr(path, '/');
+	varname = varname + (varname != NULL);
+	if (! varname)
+		varname = path;
+	return (ft_strcmp(varname, name));
+}
+
+static char *check_hashmap(const char *command)
+{
+	struct s_hashmap	*map;
+	struct s_list		*path_node;
+	char			*path;
+
+	map = getmap();
+	if (! map)
+		return (NULL);
+	path_node = hash_map_find(map, command, is_fullname);
+	if (! path_node)
+		return (NULL);
+	path = path_node->content;
+	if (access(path, F_OK) || access(path, X_OK))
+	{
+		hash_map_delete(map, path_node);
+		return (NULL);
+	}
+	return (path);
+}
+
 int	getpath(struct s_ncommand *command, struct s_env *env)
 {
 	const char	**path;
@@ -32,6 +63,9 @@ int	getpath(struct s_ncommand *command, struct s_env *env)
 		command->path = command->command;
 		return (0);
 	}
+	command->path = check_hashmap(command->command);
+	if (command->path)
+		return (0);
 	path_var = get_env_var(env, "PATH");
 	if (! path_var)
 		return (127);
@@ -40,4 +74,3 @@ int	getpath(struct s_ncommand *command, struct s_env *env)
 		return (2);
 	command->path = find_in_path(command->command, path);
 }
-*/
