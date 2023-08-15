957fcd11485bb12a53248059a731b84f2cdf2ecc
nsainton <nsainton@student.42.fr>
2023-08-15 09:47:59 +0200

    :sparkles: Finished working implementation of print_exportlist :sparkles:

--- sources/builtin/env.c
+++ sources/builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 12:02:57 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/08/14 15:37:52 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/15 09:46:44 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -310,16 +310,40 @@ static void	fill_var(char *var, const char *exported_var)
 	ft_strcat(var + len, "\"");
 }
 
-void	print_exportlist(struct s_tab *export)
+static int	compare_strings(const char **s1, const char **s2)
+{
+	return (ft_strcmp(*s1, *s2));
+}
+
+/*
+	2 blocks more in the allocation to keep room for the
+	two '"' if needed.
+*/
+int	print_exportlist(struct s_tab *export)
 {
 	char			**export_variables;
 	struct s_tab	tab;
 	size_t			i;
 	char			*new_var;
 
+	if (copy_tab(&tab, export) || \
+	insertion_sort_tab(&tab, compare_strings))
+		return (ALLOCATION_ERROR);
+	export_variables = tab.tab;
 	i = 0;
-	while (i < 
-	new_var = ft_calloc(
+	while (i < export->len)
+	{
+		new_var = ft_calloc(ft_strlen(*(export_variables + i)) + 3, \
+		sizeof **export_variables);
+		if (! new_var)
+			return (ALLOCATION_ERROR);
+		fill_var(new_var, *(export_variables + i));
+		ft_putendl_fd(new_var, STDOUT_FILENO);
+		free(new_var);
+		i ++;
+	}
+	free_node(tab.tab);
+	return (0);
 }
 /*
 	if (cmd->args[0])
