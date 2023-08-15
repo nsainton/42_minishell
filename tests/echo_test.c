05be4d0622062d29c9476063cb2ca626110a9d6e
nsainton <nsainton@student.42.fr>
2023-08-15 18:21:16 +0200

    :white_check_mark: Added test for echo builtin. Now working perfectly :white_check_mark:

---
+++ tests/echo_test.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 18:16:05 by nsainton          #+#    #+#             */
+/*   Updated: 2023/08/15 18:18:37 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv)
+{
+	int	err;
+
+	if (! argc)
+	{
+		printf("No prog name my G\n");
+		exit(1);
+	}
+	err = echo((const char **)argv + 1);
+	ft_printf("echo builtin returned : %d\n", err);
+	return (0);
+}
