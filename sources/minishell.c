116fe8cb5f3beca95dae73236c34c6d61d814c61
nsainton <nsainton@student.42.fr>
2023-08-15 19:12:58 +0200

    :construction: Starting tests for readline signal interrupt :construction:

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,12 +6,13 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:08:42 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/08/09 17:24:57 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/15 19:10:05 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/*
 static void	init(char **envp, t_data *data)
 {
 	init_sigs();
@@ -19,36 +20,61 @@ static void	init(char **envp, t_data *data)
 	data->errnum = 0;
 	data->env = get_my_env(envp);
 }
+*/
+
+void	handle(int sig)
+{
+	g_termsig = sig;
+	//printf("Salut\n");
+	rl_done = 1;
+	rl_pending_input = '\n';
+	write(0, "\n", 1);
+}
 
 volatile sig_atomic_t	g_termsig = 0;
 
-int	main(int ac, char **av, char **envp)
-{
-	char		*line;
-	t_data		data;
+extern char	**environ;
 
-	//ft_printf("Program invocation name : %s\n", program_invocation_name);
-	if (ac != 1)
+int	main(int argc, char **argv)
+{
+	char			*line;
+//	struct s_env	*env;
+
+	if (argc != 1)
 	{
 		ft_printf("%s\n", USAGE);
 		return (EXIT_FAILURE);
 	}
-	(void)av;
-	init(envp, &data);
+	(void)argv;
+	rl_catch_signals = 0;
 	signal(SIGTERM, SIG_IGN);
+	signal(SIGINT, handle);
+	/*
+	env = create_env((const char **)environ);
+	if (! env)
+		printf("PB\n");
+	*/
+	/* Handle allocation error */
 	while (1)
 	{
 		line = readline("minishell> ");
+		if (g_termsig)
+			printf("we received : %d\n", g_termsig);
 		if (!line || gc_add(line))
 		{
 			ft_putendl_fd("exit", 1);
-			exit_builtin(NULL, NULL);
+			exit(1);
+			//exit_builtin(NULL, NULL);
 			break ;
 		}
 		if (*line)
 			add_history(line);
-		commands_exec(line, &data);
-
+		printf("%s\n", line);
+		//if (commands_exec(line, &data));
+		/*
+			Import exit_message from env_test
+			exit_message(MEMORY_MESSAGE);
+		*/
 		//ft_printf("This is the last env var : %s\n", (char *)ft_lstlast(data.env->list_env)->content);
 		//free_from(ft_lstlast(data.env->list_env)->content);
 	}
