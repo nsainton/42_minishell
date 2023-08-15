b4f7ea0fcb04cb590168bc8f032ac03ef31d72fa
nsainton <nsainton@student.42.fr>
2023-08-15 17:21:14 +0200

    :sparkles: Implemented unset builtin. Needs testing :sparkles:

---
+++ sources/builtin/unset.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 17:01:10 by nsainton          #+#    #+#             */
+/*   Updated: 2023/08/15 17:20:26 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	is_opt(const char *variable)
+{
+	if (*variable != '-')
+		return (0);
+	if (*(variable + 1) && *(variable + 1) != '-')
+		return (1);
+	if (! *(variable + 1))
+		return (0);
+	return (*(variable + 2));
+}
+
+static int	option_error(const char *variable)
+{
+	ft_dprintf(STDERR_FILENO, "minishell: unset: %.2s: invalid option\n\
+unset: usage: unset\n", variable);
+	return (1);
+}
+
+int	unset(const char **args, struct s_env *env)
+{
+	if (! (args && *args))
+		return (0);
+	if (is_opt(*args))
+		return (option_error(*args));
+	while (*args)
+	{
+		del_tab_elem(env->env_list, *args, \
+		compare_names, del_string_tab);
+		del_tab_elem(env->export_list, *args, \
+		compare_names, del_string_tab);
+		i ++;
+	}
+	return (0);
+}
