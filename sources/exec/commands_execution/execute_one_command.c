361a091d172eff35fc1bd7706dee3cbddb6754dc
nsainton <nsainton@student.42.fr>
2023-08-16 12:08:53 +0200

    :construction: Save commit. Continued implementation of execute_one_command function :construction:

--- sources/exec/commands_execution/execute_one_command.c
+++ sources/exec/commands_execution/execute_one_command.c
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 12:47:55 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/16 11:28:14 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/16 12:08:35 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,38 @@ static int	pre_execution(struct s_redir *redirs, struct s_heredoc *heredocs)
 	return (0);
 }
 
-/*
+static int	find_command_path(char *command, char *command_path, struct s_tab *env)
+{
+	int	err;
+
+	err = get_command_path(command, command_path, env);
+	if (err)
+	{
+		if (err == 127)
+			ft_printf("minishell: %s: command not found\n", command);
+		exit_free_gc(err);
+	}
+	return (0);
+
+}
+
 static int	execute_file(struct s_ncommand *command, struct s_tab *env)
 {
-	
+	pid_t	child_pid;
+	int		status;
+	int		err;
+
+	child_pid = fork();
+	if (child_pid < 0)
+		return (-1);
+	if (! child_pid)
+	{
+		find_command_path(command->command, command->path, env);
+		if (execve(command->path, command->args, env->tab))
+			exit_free_gc(1);
+	}
+	wait(&status);
 }
-*/
 
 int	execute_command(struct s_ncommand *command, struct s_env *env)
 {
@@ -59,9 +85,4 @@ int	execute_command(struct s_ncommand *command, struct s_env *env)
 		save_stds(-2);
 		return (err);
 	}
-	child_pid = fork();
-	if (child_pid < 0)
-		return (1);
-	if (! child_pid)
-		execute_file(command, env);
 }
