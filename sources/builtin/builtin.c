2eecb3f6373b35c29b237403d16f18ace9797459
nsainton <nsainton@student.42.fr>
2023-08-16 09:07:58 +0200

    :sparkles: Implemented builtin function that returns the right builtin :sparkles:

--- sources/builtin/builtin.c
+++ sources/builtin/builtin.c
@@ -6,50 +6,29 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 12:51:21 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/07/27 09:49:02 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/16 09:06:44 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	exec_builtin(t_command *cmd, t_data *d)
+t_builtin	choose_builtin(const char *str)
 {
-	if (!ft_strncmp(cmd->command, "export", 7))
-		return (d->errnum = export_env(d, cmd));
-	if (!ft_strncmp(cmd->command, "unset", 6))
-		return (unset_env(d, cmd));
-	if (!ft_strncmp(cmd->command, "cd", 3))
-		return (cd(cmd, d));
-	if (!ft_strncmp(cmd->command, "exit", 5))
-		return (exit_builtin(cmd->args, &d->errnum));
-	if (!ft_strncmp(cmd->command, "env", 4))
-		return (print_env(d, cmd));
-	if (!ft_strncmp(cmd->command, "pwd", 4))
-		return (print_pwd(cmd));
-	if (!ft_strncmp(cmd->command, "echo", 5))
-		return (print_echo(d, cmd));
-	if (!ft_strncmp(cmd->command, "$?", 3))
-		return (print_exit_status(d, cmd));
-	return (127);
-}
-
-int	is_builtin(t_command *cmd, t_data *d)
-{
-	if (!cmd->command)
-		return (0);
-	if (!ft_strncmp(cmd->command, "export", 7))
-		return (1);
-	if (!ft_strncmp(cmd->command, "unset", 6))
-		return (1);
-	if (!ft_strncmp(cmd->command, "cd", 3))
-		return (1);
-	if (!ft_strncmp(cmd->command, "exit", 5))
-		return (1);
-	if (!ft_strncmp(cmd->command, "env", 4))
-		return (1);
-	if (!ft_strncmp(cmd->command, "pwd", 4))
-		return (1);
-	if (!ft_strncmp(cmd->command, "echo", 5))
-		return (1);
-	return (d->errnum = 0);
+	if (! str)
+		return (NULL);
+	if (! ft_strcmp(str, "export"))
+		return (export);
+	if (! ft_strcmp(str, "echo"))
+		return (echo);
+	if (! ft_strcmp(str, "unset"))
+		return (unset);
+	if (! ft_strcmp(str, "cd"))
+		return (cd);
+	if (! ft_strcmp(str, "env"))
+		return (env);
+	if (! ft_strcmp(str, "pwd"))
+		return (pwd);
+	if (! ft_strcmp(str, "exit"))
+		return (exit);
+	return (NULL);
 }
