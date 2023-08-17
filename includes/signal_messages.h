5351b1a371ff8ee314e6d8d9ae1db038f469db57
nsainton <nsainton@student.42.fr>
2023-08-17 14:50:39 +0200

    :construction: Save commit. Started writting signals list :construction:

---
+++ includes/signal_messages.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal_messages.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 14:43:50 by nsainton          #+#    #+#             */
+/*   Updated: 2023/08/17 14:50:23 by nsainton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SIGNAL_MESSAGES_H
+# define SIGNAL_MESSAGES_H
+# define SIGHUP_MESSAGE "Hangup"
+# define SIGINT_MESSAGE ""
+# define SIGQUIT_MESSAGE "Quit"
+# define SIGILL_MESSAGE "Illegal instruction"
+# define SIGTRAP_MESSAGE "Trace/breakpoint trap"
+# define 
+
+#endif
