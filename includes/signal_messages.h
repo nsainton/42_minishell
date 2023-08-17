e4b2ff69d44b3c23a734d23cfab80bd56bd136e1
nsainton <nsainton@student.42.fr>
2023-08-17 18:21:47 +0200

    :construction: Save commit. Continued writting signals list :construction:

--- includes/signal_messages.h
+++ includes/signal_messages.h
@@ -6,7 +6,7 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 14:43:50 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/17 14:50:23 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/17 18:21:29 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 # define SIGQUIT_MESSAGE "Quit"
 # define SIGILL_MESSAGE "Illegal instruction"
 # define SIGTRAP_MESSAGE "Trace/breakpoint trap"
+# define SIGABRT_MESSAGE "Aborted"
+# define SIGBUS_MESSAGE "Bus error"
+# define SIGFPE_MESSAGE "Floating point exception"
+# define SIGKILL_MESSAGE "Killed"
+# define SIGUSR1_MESSAGE "User defined signal 1"
+# define SIGUSR2_MESSAGE "User defined signal 2"
+# define SIGSEGV_MESSAGE "Segmentation fault"
+# define SIGALRM_MESSAGE "Alarm clock"
+# define SIGSTKFLT_MESSAGE "Stack fault"
+# define SIGXCPU_MESSAGE "CPU time limit exceeded"
+# define SIGXFSZ_MESSAGE "File size limit exceeded"
+# define SIGVTALRM_MESSAGE "Virtual timer expired"
+# define SIGPROF_MESSAGE "Profiling timer expired"
+# define SIGPOLL_MESSAGE "Power failure"
+# define SIGSYS_MESSAGE "Bad system call"
 # define 
 
 #endif
