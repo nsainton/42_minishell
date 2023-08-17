4772e0950747d6384543b3525f4122891faa3f54
nsainton <nsainton@student.42.fr>
2023-08-17 12:45:08 +0200

    :children_crossing: Added exit_message function that is more verbose about why the shell exited :children_crossing:

--- sources/exec/exit_free.c
+++ sources/exec/exit_free.c
@@ -6,15 +6,27 @@
 /*   By: nsainton <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 21:13:15 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/08 17:22:33 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/17 12:39:36 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	exit_free_gc(int status)
+void	exit_message(const int status, const char *message)
+{
+	char	*color;
+
+	color = RED;
+	if (! status)
+		color = GRN;
+	printf("%s%s%s\n", color, message, CRESET);
+	exit_free_gc(status);
+}
+
+_Noreturn void	exit_free_gc(const int status)
 {
 	close_heredoc_fds();
+	clear_list();
 	free_gc();
 	rl_clear_history();
 	exit(status);
