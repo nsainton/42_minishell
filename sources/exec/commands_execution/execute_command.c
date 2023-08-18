0a273965a5492abc320541dcd24a636ee8c74302
nsainton <nsainton@student.42.fr>
2023-08-18 13:05:19 +0200

    :sparkles: Implemented pipeline execution. Needs testing :sparkles:

--- sources/exec/commands_execution/execute_command.c
+++ sources/exec/commands_execution/execute_command.c
@@ -6,11 +6,77 @@
 /*   By: nsainton <nsainton@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 13:03:57 by nsainton          #+#    #+#             */
-/*   Updated: 2023/08/18 10:54:13 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/18 13:04:29 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	apply_pipe(const int fd_in, const int fd_out)
+{
+	if (fd_in != STDIN_FILENO)
+	{
+		if ((s_dup2(fd_in, STDIN_FILENO) < 0) || s_close(fd_in))
+			return (1);
+	}
+	if (fd_out != STDOUT_FILENO)
+	{
+		if ((s_dup2(fd_out, STDOUT_FILENO) < 0) || s_close(fd_out))
+			return (1);
+	}
+	return (0);
+}
+
+static int	pipeline_status(void)
+{
+	int		status;
+	int		tmp_status;
+	pid_t	child_pid;
+	pid_t	tmp_pid;
+
+	child_pid = 0;
+	status = 0;
+	tmp_pid = wait(&tmp_status);
+	while (tmp_pid > -1)
+	{
+		if (tmp_pid > child_pid)
+		{
+			child_pid = tmp_pid;
+			status = tmp_status;
+		}
+		tmp_pid = wait(&tmp_status);
+	}
+	return (handle_exit_status(status));
+}
+
+static int	execute_pipeline(struct s_ncommand *commands, \
+const size_t commands_nb, struct s_env *env)
+{
+	size_t	i;
+	int		err;
+
+	i = 0;
+	err = 1;
+	while (i < commands_nb)
+	{
+		pid = fork();
+		if (pid < 0)
+			break ;
+		if (! pid)
+		{
+			signal(SIGPIPE, SIG_IGN);
+			if (apply_pipe((commands + i)->input_fd, \
+			(commands + i)->output_fd))
+				exit_free_gc(1);
+			err = execute_command(commands + i, env);
+			clear_fdlist();
+			return (0);
+		}
+		i ++;
+	}
+	clear_fdlist();
+	return (pipeline_status());
+}
 
 /**
 * @brief Executes the command or pipeline received after the parsing
@@ -18,19 +84,18 @@
 *
 * @details None at the moment
 *
-* @param[in] commands : An array of struct s_ncommand that is null terminated
-* and that holds, for each of its elements, one command of the provided
-* pipeline
-* @param[in] env : The structure holding the environment that has been properly
-* formatted in an earlier function
+* @param[in] commands : An array of struct s_ncommand that is null 
+* terminated and that holds, for each of its elements, one command 
+* of the provided pipeline
+* @param[in] env : The structure holding the environment that has been 
+* properly formatted in an earlier function
 *
-* @return the status exit code of the command or pipeline provided. Might
-* not return if there is one command and this one command is exit.
+* @return the status exit code of the command or pipeline provided. 
+* Might not return if there is one command and this one command is exit.
 */
 int	execute_commands(struct s_ncommand *commands, struct s_env *env)
 {
 	size_t	commands_nb;
-	size_t	i;
 	int		err;
 
 	commands_nb = tablen(commands, sizeof * commands);
@@ -40,24 +105,11 @@ int	execute_commands(struct s_ncommand *commands, struct s_env *env)
 		clear_fdlist();
 		return (1);
 	}
-	//print_commands(commands);
-	/*
-	if (make_redirections((commands + 0)->redirs, (commands + 0)->heredocs))
+	if (commands_nb < 2)
 	{
-		ft_printf("Wtf bruv, can't even redir ? U really shell ?\n");
+		err = execute_command(commands, env);
 		clear_fdlist();
-		return (1);
+		return (err);
 	}
-	*/
-	//print_fdlist();
-	i = 0;
-	while (i < commands_nb)
-	{
-		err = execute_command(commands + i, env);
-		ft_printf("The command number %d returned : %d\n", i + 1, err);
-		keep_exit_status((err < 0) + (err >= 0) * err);
-		i ++;
-	}
-	clear_fdlist();
-	return (0);
+	return (execute_pipeline(commands, commands_nb, env));
 }
