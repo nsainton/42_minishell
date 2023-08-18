6f4197ad9c03f7cf599f9ba22730dc178a50a1af
nsainton <nsainton@student.42.fr>
2023-08-18 12:35:46 +0200

    :adhesive_bandage: Added keep exit status for ^C :adhesive_bandage:

--- sources/minishell.c
+++ sources/minishell.c
@@ -6,7 +6,7 @@
 /*   By: avedrenn <avedrenn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:08:42 by avedrenn          #+#    #+#             */
-/*   Updated: 2023/08/16 08:27:08 by nsainton         ###   ########.fr       */
+/*   Updated: 2023/08/18 12:16:42 by nsainton         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,15 @@ void	handle(int sig)
 }
 */
 
+static void	reset_termsig(void)
+{
+	printf("we received : %d\n", g_termsig);
+	keep_exit_status(g_termsig);
+	g_termsig = 0;
+	rl_replace_line("", 0);
+	rl_on_new_line();
+}
+	
 volatile sig_atomic_t	g_termsig = 0;
 
 extern char	**environ;
@@ -43,7 +52,7 @@ int	main(int argc, char **argv)
 {
 	char			*line;
 //	char			*name;
-//	struct s_env	*env;
+	struct s_env	*env;
 
 	if (argc != 1)
 	{
@@ -60,31 +69,27 @@ int	main(int argc, char **argv)
 	else
 		printf("tty name : %s\n", name);
 	*/
-	/*
 	env = create_env((const char **)environ);
 	if (! env)
-		printf("PB\n");
-	*/
-	/* Handle allocation error */
+		exit_message(1, MEM_MSG);
 	while (1)
 	{
 		line = readline("minishell> ");
 		if (g_termsig)
 		{
-			printf("we received : %d\n", g_termsig);
-			g_termsig = 0;
+			reset_termsig();
+			continue ;
 		}
 		if (!line || gc_add(line))
 		{
-			ft_putendl_fd("exit", 1);
-			exit(1);
-			//exit_builtin(NULL, NULL);
+			exit_builtin(NULL, NULL);
 			break ;
 		}
 		if (*line)
 			add_history(line);
-		printf("%s\n", line);
-		//if (commands_exec(line, &data));
+		commands_exec(line, env);
+		if (g_termsig)
+			reset_termsig();
 		/*
 			Import exit_message from env_test
 			exit_message(MEMORY_MESSAGE);
@@ -92,6 +97,6 @@ int	main(int argc, char **argv)
 		//ft_printf("This is the last env var : %s\n", (char *)ft_lstlast(data.env->list_env)->content);
 		//free_from(ft_lstlast(data.env->list_env)->content);
 	}
-	rl_clear_history();
+	exit_free_gc(1);
 	return (errno);
 }
